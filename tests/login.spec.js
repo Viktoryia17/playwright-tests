0afaf20d5391d93e90f53dd7edf1cae6cc74dead
viktoryia.yakutsevich <akucevicviktoria@gmail.com>
2025-11-09 19:03:08 +0100

    playwright hometask

---
+++ tests/login.spec.js
@@ -0,0 +1,63 @@
+// Импортируем 'test' и 'expect' из библиотеки Playwright
+const { test, expect } = require('@playwright/test');
+const { resolve } = require('path');
+
+
+
+// Описываем наш набор тестов
+test.describe('Авторизация на Sauce Demo', () => {
+
+    // Создаем тест-кейс
+    test('Пользователь должен успешно войти в систему', async ({ page }) => {
+        // 1. Переходим на страницу
+        await page.goto('https://www.saucedemo.com/');
+
+
+
+        // 2. Вводим логин
+        // Используем селектор по id
+        await page.locator('#user-name').fill('standard_user');
+
+
+        // 3. Вводим пароль
+        // Используем селектор по placeholder
+        await page.locator('[placeholder="Password"]').fill('secret_sauce');
+
+
+        // 4. Нажимаем кнопку входа
+        // Используем селектор по data-test атрибуту
+        await page.locator('[data-test="login-button"]').click();
+
+
+        // 5. Проверяем, что URL изменился и содержит нужную часть
+        await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
+
+    });
+
+    // Создаем второй тест-кейс
+    test('Пользователь должен не успешно войти в систему', async ({ page }) => {
+        // 1. Переходим на страницу
+        await page.goto('https://www.saucedemo.com/');
+
+        // 2. Вводим логин
+        // Используем селектор по id
+        await page.locator('#user-name').fill('locked_out_user');
+
+
+        // 3. Вводим пароль
+        // Используем селектор по placeholder
+        await page.locator('[placeholder="Password"]').fill('secret_sauce');
+
+        // 4. Нажимаем кнопку входа
+        // Используем селектор по data-test атрибуту
+        await page.locator('[data-test="login-button"]').click();
+
+        // 5. Ожидаем появления сообщения об ошибке
+        // Используем селектор по data-test="error"
+
+        await page.waitForSelector('.error-message-container > h3[data-test="error"]', { timeout: 5000 });
+
+
+
+    })
+});
